9b6a5e06737e0ac6b1cd4c160da329104ed329c7
Jesse Stewart <jessestewart@BVL-WX743293714.localdomain>
2021-06-01 12:37:24 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { AppConfig } from './config';
+import { FormioAppConfig } from 'angular-formio';
+import { FormioResources } from 'angular-formio/resource';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,19 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    FormioResources,
+    FormioAuthService,
+    {provide: FormioAppConfig, useValue: AppConfig},
+    {provide: FormioAuthConfig, useValue: {
+      login: {
+        form: 'user/login'
+      },
+      register: {
+        form: 'user/register'
+      }
+    }}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
